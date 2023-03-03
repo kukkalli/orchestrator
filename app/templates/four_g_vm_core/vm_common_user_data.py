43392cb9d1b2baec225960187bad569674554200
Hanif Kukkalli <kukkalli@outlook.com>
2023-03-03 20:03:38 +0100

    Shell script to start VM common user data

---
+++ app/templates/four_g_vm_core/vm_common_user_data.py
@@ -0,0 +1,99 @@
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+class CommonUserData:
+    USERDATA = """#!/bin/bash
+
+echo "Start VM User Script: $(date +"%T")" >> /home/ubuntu/initial_startup.log
+
+echo "-----------------------------------------------------------------------------------------------------" >> \
+/home/ubuntu/initial_startup.log
+echo "-----------------------------------------------------------------------------------------------------" >> \
+/home/ubuntu/initial_startup.log
+echo "-----------------------------------------------------------------------------------------------------" >> \
+/home/ubuntu/initial_startup.log
+
+while timedatectl | grep 'System clock synchronized: no' > /dev/null; do sleep 1 
+echo "waiting for clock synchronization..." done
+
+cat > /home/ubuntu/.ssh/authorized_keys << EOF
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGxlZsduAGeKqz3UhzHeXiJOsRlBQTZIyOxA0DrXso9ncDveooDqUr+Xw5XZx44nHFNjWocoQowDdaA8jj0DYEs9wF5ELGj/rm4n6a1b6tXVAlb3Vojb5C0mZfx2gUA6i5GNnNXONRttaW53XeOoD/VDM9tlgBnpa04bBQ1naTiLbQsQg== os@controller
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFJ/TSfJegktNbVbCF2L1hte8qfDtgk/zArlNq4vgEAKRePSEYnoFldlGVn5zDqnvLP2xy6WrcFUjO2TOeTnmqQ1gEzcBOjUXeYdA7LO1J8yARvvAMOk4IiuVTvGUdCIW8uDpXwfqCxqeKbSudo3LVLgt/ZcRg1QENyRLP/zqixIJoEsA== os@compute01
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACnHtnIvTXuTG2I5ngNXKUYu/h7izkEGPmfZpqIeuXcQIY0miX7k+9snBvPXKuxp5nYspOZuOzsHs4JEE3l/+ftcgHvF7w3SD5CtdTfGMhUwGHtcpWtKfj18+FiDwh9wK4m6exBChpfBTU1q14LPZBR7xg9KZULWGddugmffUK1SMoWdg== os@compute02
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAG6PCMQcMNvSA4yRmHETOYdj60fsJo4n8FOBKmlw2fJR7xWMND0DQWTVvPssv3bw1iKn5zLbx4aeVd7idKT00HsjwB4mX1/+UBVUeP/21tp50J3XsG5Pdwz4JL6LeRWvurKoU66bpBR5u0Iuo9VrJlHfn3GbCiHzke7uUt3QBmBWkxroQ== sdn@sdnc
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACU7DStTMa4kHmnZ6kiNkQHrW4CYW9kOKkR8xQa3yBvPDG0IYv0MuUJg2lY5TfdhmXNWELPYHlZxieOC60HTD/vzACoV3268mlJNYGE+ju4iQq+QXaUSwog4YkQs4aDCpylyDRJYWFe8YP97/xFOzR5P5bxCYcJZQLlwWa/+294kW29hQ== hanif@openstack
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEBCbxZcyGw+PD3S/HoPx/WKhfGOz4Mos3OGQ4Q2rvh7UpNBE4UVp/xOBcFoL0WveHI+WskQV0jKa7TnErjVwEsOAAX6O4DxaskATGq6XioPv2XmRGKb5UZ28NUCE+VLhUvnFLLn2IMiCSiNzCU8hX0rjsU6/hHjDyV01Iahq2gAY6E7Q== hanif@openstack
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAHLT0AS5MHwwJ6hX1Up5stfz361+IWA/8/MhZBH+mYA32h/Bp5hSWkQDXow4aDiHRlxVV1WLlHHup+GPBBA9XLTRwHP8gAjbP5EM4EVxR9EbDh5Hz13xcN0/n9J9rasefHS8UgTJUgRrWeNRCSAhkbNfDfSeQzk8NWlzhiwwCIUacKnzg== hanif@kukkalli
+EOF
+
+DOMAIN="@@domain@@"
+
+echo "Configure networks: $(date +"%T")" >> /home/ubuntu/initial_startup.log
+
+INTERFACES=$(find /sys/class/net -mindepth 1 -maxdepth 1 ! -name lo ! -name docker -printf "%P " -execdir cat {}/address \;)
+
+first=true
+interface_name=""
+sudo rm /etc/network/interfaces.d/50-cloud-init.cfg
+sudo -- sh -c "echo '# This file is generated from information provided by' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '# the datasource.  Changes to it will not persist across an instance.' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '# To disable cloud-init's network configuration capabilities, write a file' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '# /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg with the following:' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '# network: {config: disabled}' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '            ' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo 'auto lo' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo 'iface lo inet loopback' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo -- sh -c "echo '            ' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+
+# shellcheck disable=SC2068
+for i in ${INTERFACES[@]};
+do
+    if ${first}
+    then
+        first=false
+        interface_name=${i}
+        sudo -- sh -c "echo 'auto ${interface_name}' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+        sudo -- sh -c "echo 'iface ${interface_name} inet dhcp' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+    else
+        first=true
+        sudo -- sh -c "echo '            ' >> /etc/network/interfaces.d/50-cloud-init.cfg"
+    fi
+done
+
+sudo rm /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+sudo -- sh -c "echo '# network: {config: disabled}' >> /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg"
+sudo -- sh -c "echo 'network: {config: disabled}' >> /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg"
+echo "Configured networks: $(date +"%T")" >> /home/ubuntu/initial_startup.log
+
+# sudo systemctl restart networking.service
+
+export HOSTNAME=$(hostname -s)
+
+sudo hostnamectl set-hostname "${HOSTNAME}"."${DOMAIN}"
+
+FQDN_HOSTNAME=$(hostname)
+
+echo "Add hosts file: $(date +"%T")" >> /home/ubuntu/initial_startup.log
+
+sudo rm /etc/hosts
+cat >> /etc/hosts << EOF
+127.0.0.1  localhost
+127.0.1.1  ${FQDN_HOSTNAME} ${HOSTNAME}
+
+# The following lines are desirable for IPv6 capable hosts'
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+
+
+EOF
+
+echo "Added hosts file: $(date +"%T")" >> /home/ubuntu/initial_startup.log
+
+
+    """
---
+++ document/script_after_network_initiation.md
@@ -0,0 +1,182 @@
+# Create a script to run on boot
+
+| VM name  | IP Address |
+|----------|------------|
+| hss      | 10.10.2.96 |
+| mme      | 10.10.2.52 |
+| spgw     | 10.10.1.21 |
+
+-----------
+
+## HSS start-up on boot
+1. Create a script `start_on_boot.sh` to start the spgw on the path `/home/ubuntu/`
+   ```
+   #!/bin/bash
+   
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_hss -k
+   screen -S hss -d -m ./run_hss
+   
+   exit 0
+   ```
+
+2. Create a file `start_hss.service` under `/etc/systemd/system/`
+   ```
+   [Unit]
+   Description="Start the HSS on boot"
+   Requires=network-online.target
+   After=network-online.target
+   
+   [Service]
+   Type=oneshot
+   RemainAfterExit=yes
+   ExecStart=/home/ubuntu/start_on_boot.sh 
+   
+   [Install]
+   WantedBy=multi-user.target
+   ```
+
+3. Enable the service
+   ```
+   # systemctl enable start_hss.service
+   ```
+
+4. Verify the service is enabled
+   ```
+   # systemctl is-enabled start_hss.service
+   ```
+
+5. Start the service by the command
+   ```
+   # systemctl start start_hss.service
+   ```
+6. Create kill HSS script
+   ```
+   #!/bin/bash
+   
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_hss -k
+   
+   exit 0
+   ```
+
+
+## MME start-up on boot
+1. Create a script `start_on_boot.sh` to start the spgw on the path `/home/ubuntu/`
+   ```
+   #!/bin/bash
+
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_mme -k
+   screen -S mme -d -m ./run_mme
+   
+   exit 0
+   ```
+
+2. Create a file `start_mme.service` under `/etc/systemd/system/`
+   ```
+   [Unit]
+   Description="Start the MME on boot"
+   Requires=network-online.target
+   After=network-online.target
+   
+   [Service]
+   Type=oneshot
+   RemainAfterExit=yes
+   ExecStart=/home/ubuntu/start_on_boot.sh 
+   
+   [Install]
+   WantedBy=multi-user.target
+   ```
+
+3. Enable the service
+   ```
+   # systemctl enable start_mme.service
+   ```
+
+4. Verify the service is enabled
+   ```
+   # systemctl is-enabled start_mme.service
+   ```
+
+5. Start the service by the command
+   ```
+   # systemctl start start_mme.service
+   ```
+
+6. Create kill MME script
+   ```
+   #!/bin/bash
+   
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_mme -k
+   
+   exit 0
+   ```
+
+
+## SPGW start-up on boot
+1. Create a script `start_on_boot.sh` to start the spgw on the path `/home/ubuntu/`
+   ```
+   #!/bin/bash
+   
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_spgw -k
+   screen -S spgw -d -m ./run_spgw
+   
+   exit 0
+   ```
+
+2. Create a file `start_spgw.service` under `/etc/systemd/system/`
+   ```
+   [Unit]
+   Description="Start the SPGW on boot"
+   Requires=network-online.target
+   After=network-online.target
+   
+   [Service]
+   Type=oneshot
+   RemainAfterExit=yes
+   ExecStart=/home/ubuntu/start_on_boot.sh 
+   
+   [Install]
+   WantedBy=multi-user.target
+   ```
+
+3. Enable the service
+   ```
+   # systemctl enable start_spgw.service
+   ```
+
+4. Verify the service is enabled
+   ```
+   # systemctl is-enabled start_spgw.service
+   ```
+
+5. Start the service by the command
+   ```
+   # systemctl start start_spgw.service
+   ```
+
+6. Create kill SPGW script
+   ```
+   #!/bin/bash
+   
+   cd /home/ubuntu/openair-cn/
+   source oaienv
+   cd scripts
+   ./run_spgw -k
+   
+   exit 0
+   ```
+
